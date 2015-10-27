be5adf31540ee153e9d18a45cbb76cd4a8bba6de
Vincente <vciancio@socaldevs.com>
2015-10-27 15:49:29 -0700

    BROKEN: added more file stuff while trying to test

--- api/form.php
+++ api/form.php
@@ -8,6 +8,10 @@ function readJSON($path){
   return fread($file, filesize($path));
 }
 
+funciton generateError($reason){
+    return "{\"error\":\"true\", \"message\":\"".$reason."\"}";
+}
+
 if($_GET["name"] && $_GET["userid"] && $_GET["student_email"]){
   $userName = str_replace(' ', '_', $_GET["name"]);
   $filePath = "db/".$_GET["userid"].".".$userName;
@@ -15,7 +19,8 @@ if($_GET["name"] && $_GET["userid"] && $_GET["student_email"]){
     echo "{\"error\":\"false\", \"content\":".readJSON($path)."}";
   }
   else{
-    echo "{\"error\":\"true\"}";
+    echo generateError("Student not Found");
   }
 }
+echo var_dump($_GET);
 ?>
