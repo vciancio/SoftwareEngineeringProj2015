091685e4da2041c3b185fdf921c4a36364ce19fe
chanhi2000 <chanhi2000@gmail.com>
2015-10-30 15:58:02 -0700

    js added

--- frontend/js/temp.js
+++ frontend/js/temp.js
@@ -1,19 +1,23 @@
-window.addEvent('domready', function() {
-    // hide all but the first set
-    $$('div.hide_me').each(function(el) {
-        el.setStyle('display', 'none');
-    });
-    // call the addOne function passing the row number  
-    $$('input.add_one').each(function(el) {
-        el.addEvent('click', function(event) {
-            var id = event.target.id.substr(7, 1);
-            addOne(id);
-        });
-    });
-});
+function myFunction() {
+    var student, stdid, email;
 
-// display the next row and hide the last 'Add one' button
-function addOne(id) {
-    $('addone_'+id).setStyle('display', 'none');
-    $('recipient_'+id).setStyle('display', 'block');
-};
+    // Get the value of the input fields
+    student = document.getElementById("name").value;
+    stdid = document.getElementById("stdid").value;
+    email = document.getElementById("email ").value;
+
+    document.getElementById("demo").innerHTML = stdid;
+    document.getElementById("demo").innerHTML = stdent;
+    document.getElementById("demo").innerHTML = email;
+    
+    document.getElementById("myForm").submit();
+    
+}
+
+// The function that submits the form
+function submitForm () {
+    if(document.myform.onsubmit && !document.myform.onsubmit()) {
+        return;
+    }
+    document.myform.submit();
+}
