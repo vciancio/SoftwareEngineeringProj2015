a320668bbe47349e2ececbfed2521a976f1adf94
Vincente <vciancio@socaldevs.com>
2015-10-30 15:50:38 -0700

    added uploading to php script

--- api/form.php
+++ api/form.php
@@ -3,8 +3,18 @@
  * This is the Endpoint allows for saving and loading the form data
  */
 
+function generatePath($userid, $name){
+  return "db/".$userid.".".$name.".json";
+}
+
 function readRawJSON($path){
   return file_get_contents($path);
+}
+
+function writeJson($path, $json){
+  $file = fopen($path, "w");
+  fwrite($file,$json);
+  fclose($file); 
 }
 
 function generateError($reason){
@@ -12,14 +22,20 @@ function generateError($reason){
 }
 
 if($_GET["name"] && $_GET["userid"] && $_GET["student_email"]){
-  $userName = str_replace(' ', '_', $_GET["name"]);
-  $filePath = "db/".$_GET["userid"].".".$userName.".json";
+  $userName = strtolower(str_replace(' ', '_', $_GET["name"]));
+  $filePath = generatePath(strtolower($_GET["userid"]), $userName);
   if(file_exists($filePath)){
     echo "{\"error\":\"false\", \"content\":".readRawJSON($filePath)."}";
   }
   else{
     echo generateError("Student not Found");
   }
+}
+else if($_POST["name"] && $_POST["userid"]){
+  $username = strtolower(str_replace(' ', '_', $_POST["name"]));
+  $filePath = generatePath(strtolower($_POST["userid"]), $userName);
+  $json = file_get_contents('php://input'); 
+  writeJson($filePath, $json); 
 }
 
 
