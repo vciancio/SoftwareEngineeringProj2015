aba11e0cda777c5e7874552bb10c7d3860e27b70
Vincente <vciancio@socaldevs.com>
2015-11-02 12:19:46 -0800

    * Moved JSON javascript out of the html file. * re-added the "name" tag to the html as I was using that for the JSON

---
+++ frontend/js/utils.js
@@ -0,0 +1,41 @@
+/* JSON REQUEST / SERVER HANDLING */
+function getSelectionValue(name) {
+    return $('select[name="' + name + '"]').val();
+}
+
+/**
+  * Will Build the drop-down requirements and return a JSON
+  * object.
+  * @return a String that is formatted as: ' "key":{...}'
+  */
+function buildJSON_reqs() {
+    var json = '"reqs":"{'
+    var req_emerg = getSelectionValue("req_emerg");
+    var req_business = getSelectionValue("req_business");
+    var req_society = getSelectionValue("req_society");
+
+    json = json + "\"req_emerg\":\"" + req_emerg + "\"";
+    json = json + ", \"req_business\":\"" + req_business + "\"";
+    json = json + ", \"req_business\":\"" + req_society + "\"";
+    return json;
+}
+
+/**
+  * Will Build the JSON String that will be stored in the server
+  * @return a JSON String
+  */
+function buildJSON() {
+    var name = $('input[name="fname"]').val() + " " + $('input[name="lname"]').val();
+    var stdid = $('input[name="stdid"]').val();
+    var email = $('input[name="email"]').val();
+
+    var json = "{";
+
+    json = json + '"name":"' + name + '", "stdid":"' + stdid +
+        '", "email":"' + email + '"';
+
+    json = json + ", " + buildJSON_reqs();
+
+    json = json + "}";
+    alert(json);
+}
