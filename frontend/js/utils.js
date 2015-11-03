34afa3ff6132fcdb8b826826309beb8d68cdb859
Vincente <vciancio@socaldevs.com>
2015-11-02 23:03:03 -0800

    Added better JSON handling. Also modified the checkboxes to id standard.

--- frontend/js/utils.js
+++ frontend/js/utils.js
@@ -1,6 +1,22 @@
 /* JSON REQUEST / SERVER HANDLING */
+function isArray(obj){
+  return obj.constructor === Array;
+}
+
 function getSelectionValueByName(name) {
-    return $('select[name="' + name + '"]').val();
+  return $('select[name="' + name + '"]').val();
+}
+
+function getCheckboxValueById(id) {
+  return $("#"+id).prop('checked');
+}
+
+function buildCoenCoreReqs(){  
+  var reqs_core = new Object();
+  reqs_core.coen210 = getCheckboxValueById("coen210");
+  reqs_core.coen279 = getCheckboxValueById("coen279");
+  reqs_core.coen283 = getCheckboxValueById("coen283");
+  return reqs_core;
 }
 
 /**
@@ -8,16 +24,18 @@ function getSelectionValueByName(name) {
   * object.
   * @return a String that is formatted as: ' "key":{...}'
   */
-function buildJSON_reqs() {
-    var json = '"reqs":"{'
-    var req_emerg = getSelectionValueByName("req_emerg");
-    var req_business = getSelectionValueByName("req_business");
-    var req_society = getSelectionValueByName("req_society");
+function buildGradReqs() {
+  var json = '"reqs_grad":"{'
+  var req_emerg = getSelectionValueByName("req_emerg");
+  var req_business = getSelectionValueByName("req_business");
+  var req_society = getSelectionValueByName("req_society");
+  
+  var reqs_grad = new Object();
+  reqs_grad.req_emerg = getSelectionValueByName("req_emerg");
+  reqs_grad.req_business = getSelectionValueByName("req_business");
+  reqs_grad.req_society = getSelectionValueByName("req_society");
 
-    json = json + "\"req_emerg\":\"" + req_emerg + "\"";
-    json = json + ", \"req_business\":\"" + req_business + "\"";
-    json = json + ", \"req_business\":\"" + req_society + "\"";
-    return json;
+  return reqs_grad;
 }
 
 /**
@@ -25,18 +43,15 @@ function buildJSON_reqs() {
   * @return a JSON String
   */
 function buildJSON() {
-    var name = $('input[name="fname"]').val() + " " + $('input[name="lname"]').val();
-    var stdid = $('input[name="stdid"]').val();
-    var email = $('input[name="email"]').val();
+  var obj = new Object();
+  var json = new Object();
+  json.mName = $('input[name="fname"]').val() + " " + $('input[name="lname"]').val();
+  json.stdid = $('input[name="stdid"]').val();
+  json.email = $('input[name="email"]').val();
+  json.gradReqs = buildGradReqs();
+  json.coenReqs = buildCoenCoreReqs();
 
-    var json = "{";
+  obj.mForm = json;
 
-    json = json + '"name":"' + name + '", "stdid":"' + stdid +
-        '", "email":"' + email + '"';
-
-    json = json + ", " + buildJSON_reqs();
-
-    json = json + "}";
-    alert(json);
-    return json;
+  return JSON.stringify(obj);
 }
