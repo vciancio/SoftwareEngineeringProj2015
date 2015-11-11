6eda49302e5d064a8515fb71d0d047d99590d628
Vincente <vciancio@socaldevs.com>
2015-11-10 16:23:46 -0800

    Changed the Form to match the new style. Added the corresponding fixes to hack.js for loading and whatnot. As well, added setInputByName in utils.js

--- frontend/js/hack.js
+++ frontend/js/hack.js
@@ -3,43 +3,67 @@ function get(name){
         return decodeURIComponent(name[1]);
 }
 
+function setStudentType(type){
+    var text = "";
+    var units= 0;
+    if(type == 'undergraduate'){
+        text = "Student was a SCU Undergraduate";
+        units= 16;
+    }
+    else if(type == 'accelerated'){
+        text = "Student is an Accelerated Masters Student";
+        units= 20;
+    }
+    else {
+        text = "Student is a Transfer Student";
+        units= 9;
+    }
+
+    var html = "<div class='table-cell'><h4>" + text + "</h4></div><div class='table-cell'><h5>Max Units: " + units + " quarter units</h5></div>";
+    $('#student_type').html(html);
+}
+
 function processPrintLoadResponse(result){
+    console.log(result);
     var json = JSON.parse(result);
-    if(json.error == "true"){
+    if(json.error == "true") {
         alert("There was an error: " + json.message);
         return;
     }
 
     var obj = json.content.mForm;
-    console.log(obj);
-    var name = obj.mName.split(" ");
-    $('input[name="fname"]').val(name[0]);
-    $('input[name="lname"]').val(name[1]);
-    $('input[name="stdid"]').val(obj.stdid);
-    $('input[name="email"]').val(obj.email);
+    var name = obj.mName;
+    var name = name.split(" ");
+    setInputByName('fname', name[0]);
+    setInputByName('lname', name[1]);
+    setInputByName('stdid', obj.stdid);
+    setInputByName('email', obj.email);
 
-    // removeRow_TransferCredit();
+    removeRow_TransferCredits();
+
+    var student_type = obj.transferCredits.student_type;
+    setStudentType(student_type);
 
     //Populate the Approved Transfer Credits
-    for(var i=0; i<obj.transferCredits.length; i++){
-        var mClass = obj.transferCredits[i];
-        addRow_TransferCredit(mClass.course, mClass.institution, mClass.grade, mClass.credits);
+    for(var i=0; i<obj.transferCredits.mClasses.length; i++){
+        var mClass = obj.transferCredits.mClasses[i];
+        addRow_TransferCredits(mClass.course, mClass.institution, mClass.grade, mClass.credits);
     }
 
     removeRow_TrackUnits();
-    
+
     //Populate the Track Unit Corses
-    for(var i=0; i<obj.trackUnits.length; i++){
+    for(var i=0; i < obj.trackUnits.length; i++){
         var mClass = obj.trackUnits[i];
         addRow_TrackUnits(mClass.course, mClass.credits);
     }
 
     //Populate the Foundational Courses
     var foundationKeys = Object.keys(obj.foundationCourses);
-    for(var i=0; i<foundationKeys.length; i++){
+    for(var i=0; i < foundationKeys.length; i++){
         var courseNumber = foundationKeys[i];
         var value = obj.foundationCourses[courseNumber];
-        setCheckboxValueById(courseNumber, value);
+        setSelectionValueByName(courseNumber, value);
     }
 
     //Populate the COEN Requirements
@@ -47,7 +71,7 @@ function processPrintLoadResponse(result){
     for(var i=0; i<coenReqsKeys.length; i++){
         var courseNumber = coenReqsKeys[i];
         var value = obj.coenReqs[courseNumber];
-        setCheckboxValueById(courseNumber, value);
+        setSelectionValueByName(courseNumber, value);
     }
 
     //Populate the Graduate Core Requirements
@@ -55,17 +79,11 @@ function processPrintLoadResponse(result){
     for(var i=0; i<gradCoreKeys.length; i++){
         var requirement = gradCoreKeys[i];
         var value = obj.gradReqs[requirement];
-        setSelctionValueByName(requirement, value);
+        setSelectionValueByName(requirement, value);
     }
 
-    totalUnitAnalysis();
-    
-    var millisecondsToWait = 1000;
-    setTimeout(function() {
-        window.print();
-    }, millisecondsToWait);
+totalUnitAnalysis();
 }
-
 
 $(document).ready(function () {
     var name = get('name');
